d1be433d3f5299f36ae548d635f46f492e7e3d10
“Jose <Gutierrez@Gutierrezs-MacBook-Air.local>
2017-07-11 21:29:24 -0500

    added more products

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,22 +11,22 @@ class User < ApplicationRecord
 
 		# Lemonade
 		if current_user_answers.where(answer_node_id: 1).exists?
-			# num of cups
-			if current_user_answers.where(answer_node_id: 2).exists?
-				num_cups = 1
-			elsif current_user_answers.where(answer_node_id: 3).exists?
-				num_cups = 2
-			elsif current_user_answers.where(answer_node_id: 4).exists?
-				num_cups = 3
-			end
+			# # num of cups
+			# if current_user_answers.where(answer_node_id: 2).exists?
+			# 	# num_cups = 1
+			# elsif current_user_answers.where(answer_node_id: 3).exists?
+			# 	# num_cups = 2
+			# elsif current_user_answers.where(answer_node_id: 4).exists?
+			# 	# num_cups = 3
+			# end
 
 			# size
 			if current_user_answers.where(answer_node_id: 5).exists?
-				CartedProduct.create(user_id: id, product_id: 1, quantity: num_cups, status: "carted")
+				CartedProduct.create(user_id: id, product_id: 1, quantity: 1, status: "carted")
 			elsif current_user_answers.where(answer_node_id: 6).exists?
-				CartedProduct.create(user_id: id, product_id: 2, quantity: num_cups, status: "carted")
+				CartedProduct.create(user_id: id, product_id: 2, quantity: 2, status: "carted")
 			elsif current_user_answers.where(answer_node_id: 7).exists?
-				CartedProduct.create(user_id: id, product_id: 3, quantity: num_cups, status: "carted")
+				CartedProduct.create(user_id: id, product_id: 3, quantity: 3, status: "carted")
 			end
 		end
 		
